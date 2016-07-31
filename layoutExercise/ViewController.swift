cc78b8fc8a758ba2086b8532d624abbf7e87a746
ABnyc <arpita.nyc@gmail.com>
2016-07-30 21:14:02 -0400

    Initial Commit

---
+++ layoutExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  layoutExercise
+//
+//  Created by Arpita Bhatia on 7/30/16.
+//  Copyright © 2016 Arpita Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
